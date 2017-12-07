076bc31c498f3910981cbd3fb6fcbe03dea6e6a7
David Indrawes <david.indrawes@gmail.com>
2017-12-07 15:44:25 +0000

    adding base configurate class in the common and configurations customs exceptions

---
+++ Src/Tools/BaseConfigurationsReader/Exceptions/ManadatoryConfigurationPropertyNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fodiz.Common.Tools.BaseConfigurationsReader.Exceptions
+{
+    class ManadatoryConfigurationPropertyNotFoundException : Exception
+    {
+
+        public ManadatoryConfigurationPropertyNotFoundException (string configurationFileName, string DirectoryOfConfigurationFile, string missingPropertyKey,Exception innerException = null): base (CreateMessage(configurationFileName, DirectoryOfConfigurationFile, missingPropertyKey), innerException)
+        {
+        }
+
+        private static string CreateMessage(string configurationFileName, string DirectoryOfConfigurationFile, string missingPropertyKey)
+        {
+            return string.Format("The key: \"{0}\", didn't match with any of the properties keys in the configuration file: \"{1}\" found in the directory: \"{2}\", are you sure you included this configuration in the config file ???", missingPropertyKey,configurationFileName,DirectoryOfConfigurationFile);
+        }
+    }
+}
---
+++ Src/Tools/BaseConfigurationsReader/JsonBaseConfigurationsReader.cs
@@ -0,0 +1,56 @@
+﻿using Dudes.Logger;
+using Fodiz.Common.Tools.BaseConfigurationsReader.Exceptions;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Fodiz.Common.Tools.BaseConfigurationsReader
+{
+    public class JsonBaseConfigurationsReader
+    {
+        protected readonly string configurationFileName;
+        private readonly IConfigurationRoot configuration;
+        private readonly ILogger _logger;
+
+        public JsonBaseConfigurationsReader(ILogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+        }
+
+        protected string GetOptionalConfiguration(string key)
+        {
+            _logger.DEBUG(string.Format("Reading optional configuration for Key: \"{0}\", from the file \"{1}\" from the directory {2}", key, configurationFileName, Directory.GetCurrentDirectory()));
+            string value = configuration[key];
+
+            _logger.DEBUG(string.Format("Value: \"{0}\", found for the key: \"{1}\"", value, key));
+            return value;
+        }
+
+        protected string GetManadatoryConfiguration(string key)
+        {
+            _logger.DEBUG(string.Format("Reading manadatory configuration for Key: \"{0}\", from the file \"{1}\" from the directory {2}", key, configurationFileName, Directory.GetCurrentDirectory()));
+            string value = configuration[key];
+            
+            if(value==null)
+            {
+                _logger.ERROR(string.Format("Manadatory Configuration, The key: \"{0}\", didn't match with any of the properties keys in the configuration file: \"{1}\" found in the directory: \"{2}\", are you sure you included this configuration in the config file ???", key, configurationFileName, Directory.GetCurrentDirectory()));
+                throw new ManadatoryConfigurationPropertyNotFoundException(configurationFileName, Directory.GetCurrentDirectory(), key);
+            }
+            else
+            {
+                _logger.DEBUG(string.Format("Value: \"{0}\", found for the key: \"{1}\"", value, key));
+                return value;
+            }
+            
+        }
+
+        protected void BuildConfiguration()
+        {
+
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile(configurationFileName);
+        }
+    }
+}
