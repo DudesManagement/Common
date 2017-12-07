c70776d086bdc5a24c17aef497b113c3ca4119f9
David Indrawes <david.indrawes@gmail.com>
2017-12-07 16:23:13 +0000

    adding unit test project and fixes in the .nuspec file

---
+++ Tests/Unit.Tests/Configuration.Unit.Tests/Tools.Unit.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tools.Unit.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
